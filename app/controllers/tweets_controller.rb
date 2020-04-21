6a5ed1c2199e09fc79758c835736ae129d83e4cb
Bouloute <annaick1995@gmail.com>
2020-04-20 22:01:00 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,46 @@
 class TweetsController < ApplicationController
 
+    #Users can create new tweets only if logged in
+    get '/tweets/new' do 
+        if logged_in?(session)
+            erb :'/tweets/new'
+        else
+            erb :'/users/login'
+        end
+    end
+
+    post '/tweets/new' do  
+        #Blank tweets are not saved
+        if !params[:content].empty?
+            @tweet = Tweet.create(:content => params[:content])
+            @tweet.user = current_user(session)
+            @tweet.save
+            redirect "/tweets"
+        else
+            redirect "/error"
+        end
+    end
+    
+    
+    get "/tweets/:id/edit" do 
+        @tweet = Tweet.find_by_id(params[:id])
+        erb :'/tweets/edit'
+    end
+    
+    patch '/tweets/:id/edit' do
+        @tweet = Tweet.find_by_id(params[:id])
+        
+        #Users can edit/delete only their tweets
+        if @tweet.user == current_user(session)
+            @tweet.content = params[:content]
+            @tweet.save
+        end
+        redirect "/tweets"
+    end
+    
+    #Users can view other users tweets
+    get "/tweets" do 
+        erb :'/tweets/tweets'
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,55 @@
 class UsersController < ApplicationController
 
+    #Users can create an account
+    get '/users/new' do
+        erb :'users/new'
+    end
+
+    #New accounts must have a username, email and password
+    post '/users/new' do 
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect to "/error"
+        end
+
+        user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        binding.pry
+        if user.save
+          #login
+          redirect to "/tweets"#redirect to all tweets?
+        else
+          redirect "/error"
+        end
+    end
+
+    
+    #Users can log in and log out
+    get '/users/login' do
+        erb :'users/login'
+    end
+
+    post '/users/login' do 
+        user = User.find_by(:email => params[:email])
+ 
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/tweets" #view all tweets?
+        else
+            redirect "/error"
+        end
+    end
+
+    get '/users/logout' do
+        erb :'users/logout'
+    end
+
+    post '/users/logout' do 
+        session.destroy
+        redirect "/" #view all tweets?
+    end
+
+    
+    
 
 end
+
+
