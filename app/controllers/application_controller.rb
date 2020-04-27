9366747690ae95eacff1d9d7f3744d508bb6d372
Charles Pelton <charliejp0311@gmail.com>
2020-04-27 19:22:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,12 +23,33 @@ class ApplicationController < Sinatra::Base
 
   post '/signup' do
     if !params["username"].empty? && !params["password"].empty? && !params["email"].empty?
-      user = User.create(params["user"])
+      user = User.create(params)
       session[:user_id] = user.id
+      # binding.pry
       redirect to '/tweets'
     else
       redirect to '/signup'
     end
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets' do
+    @user = User.find_by_id(session["user_id"])
+    @tweets = @user.tweets
+    erb :'tweets/tweets'
   end
 
   helpers do
