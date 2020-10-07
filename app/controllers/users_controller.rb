aacfcbd3412e6df5b4f1ec9f872cf0ea46f3060f
DCTorres87 <cruz_danielle_torres@yahoo.com>
2020-10-06 22:55:56 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
         user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
         
         if user.save
-            session[:user_id] = user.id
+            # session[:user_id] = user.id
             redirect "/tweets"
         else
             redirect "/signup"
